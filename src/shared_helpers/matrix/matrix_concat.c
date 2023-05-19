97ac6d39794a1f1ad59c8d64ef563be0f62fc571
perly <aittaleb108@gmail.com>
2023-05-19 18:59:46 +0100

    clean up test strings

--- src/shared_helpers/matrix/matrix_concat.c
+++ src/shared_helpers/matrix/matrix_concat.c
@@ -22,9 +22,6 @@ char	**matrix_concat(char **matrix, char **back)
 
 	matrix_len = count_matrix(matrix);
 	back_len = count_matrix(back);
-	ft_printf("matrix_len = %d\n", matrix_len);
-	ft_printf("back_len = %d\n", back_len);
-	print_matrix(back, ' ');
 	result = ft_calloc((matrix_len + back_len + 1), sizeof(char *));
 	if (!result)
 		return (NULL);
@@ -37,16 +34,10 @@ char	**matrix_concat(char **matrix, char **back)
 	i = 0;
 	while (i < back_len)
 	{
-		ft_printf("start\n");
-		ft_printf("%s\n", result[matrix_len + i]);
-		ft_printf("%s\n", back[i]);
-		ft_printf("shiit1\n");
 		tmp = ft_strdup(back[i]);
-		ft_printf("shiit2\n");
 		result[matrix_len + i] = tmp;
 		i++;
 	}
-	ft_printf("out\n");
 	result[matrix_len + back_len] = NULL;
 	return (free_matrix(matrix), result);
 }
