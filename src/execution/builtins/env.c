35a2b713949fb48d92ad962be995efa2b20183fd
Hanane Asseraou <asseraouhanane@gmail.com>
2023-05-21 15:29:50 +0100

    env

--- src/execution/builtins/env.c
+++ src/execution/builtins/env.c
@@ -6,8 +6,15 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 11:07:03 by hasserao          #+#    #+#             */
-/*   Updated: 2023/05/04 01:28:01 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/05/21 15:27:52 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+char **ft_env(t_exec_context *context)
+{
+	t_env *env;
+	print_env(env,1);
+	return(env->env_array);
+}
--- src/execution/exec.c
+++ src/execution/exec.c
@@ -6,9 +6,15 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 16:48:35 by hasserao          #+#    #+#             */
-/*   Updated: 2023/05/02 16:48:51 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/05/21 15:20:31 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void exec_builtins(t_doubly_lst *list)
+{
+	char *env;
+	if(list->cmd == 'env')
+		ft_env(list->cmd);
+}
