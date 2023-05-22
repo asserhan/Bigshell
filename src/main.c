947181aea5fd5b9f98d61add1631c36418275ec2
Hanane Asseraou <asseraouhanane@gmail.com>
2023-05-22 02:53:40 +0100

    [wip] parse export

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 02:32:00 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/05/22 01:21:08 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/05/22 01:51:11 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	main(int ac, char **av, char **env)
 {
 	t_exec_context	exContext;
 	char			*input;
-	int i=0;
 	(void)ac;
 	if (init_data(&exContext, av, env))
 		exit(1);
@@ -27,11 +26,11 @@ int	main(int ac, char **av, char **env)
 		input = readline("minishell $ ");
 		if (!*input || pars_input(&exContext, input))
 			continue ;
-		while (exContext.cmds->args[i])
-		{
-			printf("%s\n", exContext.cmds->args[i]);
-			i++;
-		}
+		// while (exContext.cmds->args[i])
+		// {
+		// 	printf("%s\n", exContext.cmds->args[i]);
+		// 	i++;
+		// }
 		exec_builtins(&exContext);
 	}
 	return (0);
