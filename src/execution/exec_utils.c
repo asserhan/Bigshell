311b9ebf6049220ab5b6a977bbe60ea44f6826c7
Hanane asseraou <asseraouhanane@gmail.com>
2023-06-03 16:42:53 +0100

    gitignore

---
+++ src/execution/exec_utils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 14:35:19 by hasserao          #+#    #+#             */
+/*   Updated: 2023/06/02 22:11:48 by hasserao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	ft_get_path(t_exec_context *exContext)
+{
+	char *tmp;
+	t_env_variable *envp;
+	int i;
+	exContext->cmd_paths = NULL;
+	envp = exContext->env->first;
+	i = -1;
+	//print_env(exContext->env,1);
+	while (envp)
+	{
+		exContext->paths = search_env_elem(exContext->env, "PATH");
+		envp = envp->next;
+		
+	}
+	if (!exContext->paths)
+		return ;
+	exContext->cmd_paths = ft_split(exContext->paths->content, ':');
+	if (!exContext->cmd_paths)
+		ft_msg_error("Error\n",1);
+	while (exContext->cmd_paths[++i])
+	{
+		tmp = exContext->cmd_paths[i];
+		exContext->cmd_paths[i] = ft_strjoin(tmp, "/");
+		if (!exContext->cmd_paths[i])
+			ft_msg_error("Error\n",1);
+		free(tmp);
+	}
+}
+char *ft_get_cmd_path(t_exec_context *exContext)
+{
+	char *my_cmd;
+	int i;
+	i = -1;
+	if(!exContext->cmd_paths)
+		return(exContext->cmds->cmd); // update
+	while (exContext->cmd_paths[++i])
+	{
+		my_cmd  = ft_strjoin(exContext->cmd_paths[i], exContext->cmds->cmd);
+		if(access(my_cmd, F_OK | X_OK) == 0)
+		{
+			free(exContext->cmds->cmd);
+			return(my_cmd);
+		}
+		free(my_cmd);	
+	}
+	return(NULL);
+}
---
+++ src/execution/process.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 14:35:43 by hasserao          #+#    #+#             */
+/*   Updated: 2023/06/03 16:41:37 by hasserao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	ft_execute_child(t_exec_context *exContext)
+{
+	if (dup2(exContext->cmds->in, STDIN_FILENO) == -1)
+		ft_msg_error("dup2", 1);
+	if (dup2(exContext->cmds->out, STDOUT_FILENO) == -1)
+		ft_msg_error("dup2", 1);
+	if (exContext->paths && ft_strchr(exContext->cmds->cmd, '/') == NULL)
+		exContext->cmds->cmd = ft_get_cmd_path(exContext);
+	if (!exContext->cmds->cmd)
+	{
+        ///ft_printf("**%s**\n", exContext->cmds->cmd);
+		free_matrix(exContext->cmds->args);
+		put_error_ex("minishell: ", exContext->cmds->cmd, "command not found\n",
+				127);
+		free(exContext->cmds->cmd);
+        exit(127);
+	}
+	execve(exContext->cmds->cmd, exContext->cmds->args, exContext->env->env_array);
+	ft_msg_error("execve", 127);
+	exit(127);
+}
