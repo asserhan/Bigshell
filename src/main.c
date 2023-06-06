de090a3639411722573d1ffef1e7b98bfa4ae84a
Hanane asseraou <asseraouhanane@gmail.com>
2023-06-06 21:39:54 +0100

    close fds

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 02:32:00 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/06/06 20:52:39 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/06/06 21:28:36 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ int	main(int ac, char **av, char **env)
 		//print_matrix(exContext.env->env_array,'\n');
 		//exec_builtins(&exContext);
 		execution(&exContext);
+		
 		//wait(NULL);
 	}
 	return (0);
