9a1af120d6d26e8b2cae588e4e2f5363abd9a364
PROXY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-06-07 13:15:42 +0100

    [norm] header

--- src/shared_helpers/matrix/count_matrix.c
+++ src/shared_helpers/matrix/count_matrix.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   count_matrix.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 23:04:59 by otait-ta          #+#    #+#             */
+/*   Updated: 2023/06/06 23:05:02 by otait-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../includes/minishell.h"
 
-int count_matrix(char **arr)
+int	count_matrix(char **arr)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    if (!arr)
-        return 0;
-    while (*arr != NULL)
-    {
-        count++;
-        arr++;
-    }
-    return count;
+	count = 0;
+	if (!arr)
+		return (0);
+	while (*arr != NULL)
+	{
+		count++;
+		arr++;
+	}
+	return (count);
 }
