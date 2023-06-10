5b90c9a68819489dea570b8753423ada76b1bc62
PROXY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-06-10 11:32:04 +0100

    [fix] delete a node if have an error

--- src/shared_helpers/doubly_list/d_lstdelone.c
+++ src/shared_helpers/doubly_list/d_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 09:17:31 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/06/10 10:34:26 by otait-ta         ###   ########.fr       */
+/*   Updated: 2023/06/10 11:17:05 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ void	d_lstdelone(t_doubly_lst **head, t_doubly_lst *nodeToDelete)
 		nodeToDelete->prev->next = nodeToDelete->next;
 	if (nodeToDelete->next != NULL)
 		nodeToDelete->next->prev = nodeToDelete->prev;
+	if (nodeToDelete->cmd)
+		free(nodeToDelete->cmd);
+	if (nodeToDelete->args)
+		free_matrix(nodeToDelete->args);
 	free(nodeToDelete);
 }
