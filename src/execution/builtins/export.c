a8da21f85fe79ee456573e8021fe218c96b0dc72
Hanane asseraou <asseraouhanane@gmail.com>
2023-06-13 13:03:50 +0100

    [fix] leaks in export

--- src/execution/builtins/export.c
+++ src/execution/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 11:07:12 by hasserao          #+#    #+#             */
-/*   Updated: 2023/06/13 12:25:48 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/06/13 12:59:54 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,25 @@ char	*ft_elimine_char(char *str, char c)
 	new[i] = '\0';
 	return (new);
 }
+char	*ft_joining(char const *s1, char const *s2)
+{
+	char	*p;
+	size_t	len;
+
+	if (!s1)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	p = (char *)malloc(len + 1 * sizeof(char));
+	if (!p)
+		return (NULL);
+	else
+	{
+		ft_strlcpy(p, s1, ft_strlen(s1) + 1);
+		ft_strlcat(p, s2, len + 1);
+		free((char *)s1);
+	}
+	return (p);
+}
 void	_export_variable(char *arg, t_env *env)
 {
 	t_env_variable	*tmp;
@@ -103,9 +122,9 @@ void	_export_variable(char *arg, t_env *env)
 			tmp = create_env_elem(var);
 		else
 		{
-			s = ft_strjoin(s, "=");
-			s = ft_strjoin(s, tmp->content);
-			s = ft_strjoin(s, str);
+			s = ft_joining(s, "=");
+			s = ft_joining(s, tmp->content);
+			s = ft_joining(s, str);
 			delete_env_elem(env, tmp);
 			tmp = create_env_elem(s);
 		}
