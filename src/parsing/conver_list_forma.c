08a8479e47d86b70686dee903a6e48846da5ff72
PROXY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-06-17 09:48:42 +0100

    [fix] sig in echo "ls > ok"

--- src/parsing/conver_list_forma.c
+++ src/parsing/conver_list_forma.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   conver_list_forma.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hasserao <hasserao@student.42.fr>          +#+  +:+       +#+        */
+/*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 18:43:12 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/06/16 19:27:04 by hasserao         ###   ########.fr       */
+/*   Updated: 2023/06/17 09:43:42 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,10 +72,10 @@ int	fill_cmd_and_args(t_doubly_lst **head, t_doubly_lst **list,
 
 	while (*list && ft_strcmp((*list)->cmd, "|") && *node)
 	{
-		if (find_char_index((*list)->cmd, "><") >= 0)
+		if (find_char_index((*list)->cmd, "><") >= 0 && !(*list)->have_quotes)
 		{
 			if (fill_in_out(list, node, exContext, &red_type))
-				return (d_lstclear(head),d_lstclear(list), 1);
+				return (d_lstclear(head), d_lstclear(list), 1);
 			continue ;
 		}
 		if ((*node)->cmd[0] == '\0')
